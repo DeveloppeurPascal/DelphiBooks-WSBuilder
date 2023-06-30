1bbfb3aa9ddaf2b89c0cd0e00a46c53022aef76d
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-06-30 16:32:59 +0200

    finished the frmMain form with 3 buttons (about dialog, build, close)

--- src/DelphiBooksWebSiteBuilder.dpr
+++ src/DelphiBooksWebSiteBuilder.dpr
@@ -4,7 +4,10 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   fMain in 'fMain.pas' {fmrMain},
-  uDMAboutBoxImage in 'uDMAboutBoxImage.pas' {DMAboutBoxImage: TDataModule};
+  uDMAboutBoxImage in 'uDMAboutBoxImage.pas' {DMAboutBoxImage: TDataModule},
+  Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
+  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
+  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas';
 
 {$R *.res}
 
--- src/fMain.pas
+++ src/fMain.pas
@@ -3,13 +3,22 @@ unit fMain;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  uDMAboutBoxImage, Olf.FMX.AboutDialog;
+  uDMAboutBoxImage, Olf.FMX.AboutDialog, FMX.Controls.Presentation,
+  FMX.StdCtrls;
 
 type
   TfmrMain = class(TForm)
     OlfAboutDialog1: TOlfAboutDialog;
+    btnAbout: TButton;
+    btnClose: TButton;
+    btnBuild: TButton;
+    procedure btnCloseClick(Sender: TObject);
+    procedure btnAboutClick(Sender: TObject);
+    procedure OlfAboutDialog1URLClick(const AURL: string);
+    procedure btnBuildClick(Sender: TObject);
   private
     { Déclarations privées }
   public
@@ -22,5 +31,27 @@ var
 implementation
 
 {$R *.fmx}
+
+uses u_urlOpen;
+
+procedure TfmrMain.btnAboutClick(Sender: TObject);
+begin
+  OlfAboutDialog1.execute;
+end;
+
+procedure TfmrMain.btnBuildClick(Sender: TObject);
+begin
+// TODO : à compléter
+end;
+
+procedure TfmrMain.btnCloseClick(Sender: TObject);
+begin
+  close;
+end;
+
+procedure TfmrMain.OlfAboutDialog1URLClick(const AURL: string);
+begin
+  url_Open_In_Browser(AURL);
+end;
 
 end.
