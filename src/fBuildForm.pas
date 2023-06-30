6439db9bce96e73a0648d8e8579eb768aceb67ef
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-06-30 17:46:24 +0200

    added the needed procedures for the builder algorithm added log(), logTitle(), logError() and DebugLog() methods implemented getFolders() method

--- src/fBuildForm.pas
+++ src/fBuildForm.pas
@@ -12,8 +12,13 @@ uses
   FMX.Controls,
   FMX.Forms,
   FMX.Graphics,
-  FMX.Dialogs, FMX.Memo.Types, FMX.StdCtrls, FMX.Controls.Presentation,
-  FMX.ScrollBox, FMX.Memo;
+  FMX.Dialogs,
+  FMX.Memo.Types,
+  FMX.StdCtrls,
+  FMX.Controls.Presentation,
+  FMX.ScrollBox,
+  FMX.Memo,
+  DelphiBooks.DB.Repository;
 
 type
   TfrmBuildForm = class(TForm)
@@ -22,10 +27,25 @@ type
     procedure btnCloseClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
+    procedure LoadRepositoryDatabase(DBFolder: string;
+      var DB: TDelphiBooksDatabase);
+    procedure UpdateNewObjectsProperties(DB: TDelphiBooksDatabase);
+    procedure SaveRepositoryDatabase(DBFolder: string;
+      DB: TDelphiBooksDatabase);
+    procedure BuildWebSitePages(TemplateFolder, SiteFolder: string;
+      DB: TDelphiBooksDatabase);
+    procedure BuildWebSiteAPI(TemplateFolder, SiteFolder: string;
+      DB: TDelphiBooksDatabase);
+    procedure BuildWebSiteImages(DBFolder, SiteFolder: string;
+      DB: TDelphiBooksDatabase);
+    procedure getFolders(var DBFolder, TemplateFolder, SiteFolder: string);
     { Déclarations privées }
   public
     { Déclarations publiques }
+    procedure debuglog(ATxt: string);
     procedure log(ATxt: string);
+    procedure logTitle(ATxt: string);
+    procedure logError(ATxt: string);
     procedure Execute;
   end;
 
@@ -36,17 +56,164 @@ implementation
 
 {$R *.fmx}
 
+uses
+  System.IOUtils,
+  DelphiBooks.Classes;
+
 procedure TfrmBuildForm.btnCloseClick(Sender: TObject);
 begin
   Close;
 end;
 
 procedure TfrmBuildForm.Execute;
+var
+  DB: TDelphiBooksDatabase;
+  DBFolder, TemplateFolder, SiteFolder: string;
 begin
+  DB := nil;
+  try
+    getFolders(DBFolder, TemplateFolder, SiteFolder);
+
+    LoadRepositoryDatabase(DBFolder, DB);
+    try
+      UpdateNewObjectsProperties(DB);
+      BuildWebSitePages(TemplateFolder, SiteFolder, DB);
+      BuildWebSiteAPI(TemplateFolder, SiteFolder, DB);
+      BuildWebSiteImages(DBFolder, SiteFolder, DB);
+      SaveRepositoryDatabase(DBFolder, DB);
+    finally
+      DB.free;
+    end;
+  except
+    on e: exception do
+    begin
+      logError(e.Message);
+      raise;
+    end;
+  end;
+end;
+
+procedure TfrmBuildForm.getFolders(var DBFolder, TemplateFolder,
+  SiteFolder: string);
+var
+  ProgFolder: string;
+  RootSiteRepositoryFolder: string;
+begin
+  // get exe file folder
+  ProgFolder := tpath.GetDirectoryName(paramstr(0));
+  if ProgFolder.isempty then
+    raise exception.Create('Can''t extract program directory path.');
+  if not tdirectory.Exists(ProgFolder) then
+    raise exception.Create('Can''t find folder "' + ProgFolder + '".');
+  debuglog(ProgFolder);
+
+  // get DelphiBooks-WebSite folder depending on DEBUG/RELEASE version
+{$IFDEF RELEASE}
+  // the exe file is in "/site-builder" folder of the repository
+  RootSiteRepositoryFolder := tpath.Combine(ProgFolder, '..');
+{$ELSE}
+  // the compiled exe is in /src/Win32/debug (or else)
+  // the web site repository is in /lib-externes/DelphiBooks-WebSite
+  RootSiteRepositoryFolder := tpath.Combine(ProgFolder, '..');
+  RootSiteRepositoryFolder := tpath.Combine(RootSiteRepositoryFolder, '..');
+  RootSiteRepositoryFolder := tpath.Combine(RootSiteRepositoryFolder, '..');
+  RootSiteRepositoryFolder := tpath.Combine(RootSiteRepositoryFolder,
+    'lib-externes');
+  RootSiteRepositoryFolder := tpath.Combine(RootSiteRepositoryFolder,
+    'DelphiBooks-WebSite');
+{$ENDIF}
+  if RootSiteRepositoryFolder.isempty then
+    raise exception.Create('Can''t define root repository path.');
+  if not tdirectory.Exists(RootSiteRepositoryFolder) then
+    raise exception.Create('Can''t find folder "' +
+      RootSiteRepositoryFolder + '".');
+  debuglog(RootSiteRepositoryFolder);
+
+  // Database is in /database/datas folder in the WebSite repository
+  DBFolder := tpath.Combine(RootSiteRepositoryFolder, 'database');
+  DBFolder := tpath.Combine(DBFolder, 'datas');
+  if DBFolder.isempty then
+    raise exception.Create('Can''t define database path.');
+  if not tdirectory.Exists(DBFolder) then
+    raise exception.Create('Can''t find folder "' + DBFolder + '".');
+  debuglog(DBFolder);
+
+  // Templates are in /site-templates/templates folder
+  TemplateFolder := tpath.Combine(RootSiteRepositoryFolder, 'site-templates');
+  TemplateFolder := tpath.Combine(TemplateFolder, 'templates');
+  if TemplateFolder.isempty then
+    raise exception.Create('Can''t define templates path.');
+  if not tdirectory.Exists(TemplateFolder) then
+    raise exception.Create('Can''t find folder "' + TemplateFolder + '".');
+  debuglog(TemplateFolder);
+
+  // The generated pages are in /docs folder
+  SiteFolder := tpath.Combine(RootSiteRepositoryFolder, 'docs');
+  if SiteFolder.isempty then
+    raise exception.Create('Can''t define web site path.');
+  if not tdirectory.Exists(SiteFolder) then
+    raise exception.Create('Can''t find folder "' + SiteFolder + '".');
+  debuglog(SiteFolder);
+end;
+
+procedure TfrmBuildForm.BuildWebSiteImages(DBFolder, SiteFolder: string;
+  DB: TDelphiBooksDatabase);
+begin
+  // build the images thumbs
+  logTitle('Build the images');
   // TODO : à compléter
-  log('test before');
-  sleep(2000);
-  log('test after');
+  log('Finished');
+end;
+
+procedure TfrmBuildForm.BuildWebSiteAPI(TemplateFolder, SiteFolder: string;
+  DB: TDelphiBooksDatabase);
+begin
+  // build the API
+  logTitle('Build the API files');
+  // TODO : à compléter
+  log('Finished');
+end;
+
+procedure TfrmBuildForm.BuildWebSitePages(TemplateFolder, SiteFolder: string;
+  DB: TDelphiBooksDatabase);
+begin
+  // build the website
+  logTitle('Build the web pages');
+  // TODO : à compléter
+  log('Finished');
+end;
+
+procedure TfrmBuildForm.debuglog(ATxt: string);
+begin
+{$IFDEF DEBUG}
+  log(ATxt);
+{$ENDIF}
+end;
+
+procedure TfrmBuildForm.SaveRepositoryDatabase(DBFolder: string;
+  DB: TDelphiBooksDatabase);
+begin
+  // save the new objects in the repository database
+  logTitle('Save the changed objects in the repository database');
+  // TODO : à compléter
+  log('Finished');
+end;
+
+procedure TfrmBuildForm.UpdateNewObjectsProperties(DB: TDelphiBooksDatabase);
+begin
+  // update the missing id properties (from new objects)
+  logTitle('Fill new objects IDs');
+  // TODO : à compléter
+  log('Finished');
+end;
+
+procedure TfrmBuildForm.LoadRepositoryDatabase(DBFolder: string;
+  var DB: TDelphiBooksDatabase);
+begin
+  // load the repository database
+  logTitle('Load the repository database');
+  // TODO : à compléter
+  log('Finished');
 end;
 
 procedure TfrmBuildForm.FormCreate(Sender: TObject);
@@ -79,6 +246,36 @@ begin
     procedure
     begin
       mmoLog.lines.add(ATxt);
+      mmoLog.GoToTextEnd;
+    end);
+end;
+
+procedure TfrmBuildForm.logError(ATxt: string);
+begin
+  tthread.Synchronize(nil,
+    procedure
+    begin
+      mmoLog.lines.add('');
+      mmoLog.lines.add('*****************');
+      mmoLog.lines.add('*** EXCEPTION ***');
+      mmoLog.lines.add('*****************');
+      mmoLog.lines.add(ATxt);
+      mmoLog.lines.add('*****************');
+      mmoLog.lines.add('');
+      mmoLog.GoToTextEnd;
+    end);
+end;
+
+procedure TfrmBuildForm.logTitle(ATxt: string);
+begin
+  tthread.Synchronize(nil,
+    procedure
+    begin
+      mmoLog.lines.add('');
+      mmoLog.lines.add('****************************************');
+      mmoLog.lines.add(ATxt);
+      mmoLog.lines.add('****************************************');
+      mmoLog.lines.add('');
       mmoLog.GoToTextEnd;
     end);
 end;
