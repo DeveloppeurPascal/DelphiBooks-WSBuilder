9fdcac6696f951bbdf9d568cbd6e04e12d7dd869
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-06-30 17:24:09 +0200

    added common Delphi Books units (classes en DB.repository)

--- src/DelphiBooksWebSiteBuilder.dpr
+++ src/DelphiBooksWebSiteBuilder.dpr
@@ -3,17 +3,20 @@ program DelphiBooksWebSiteBuilder;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  fMain in 'fMain.pas' {fmrMain},
+  fMain in 'fMain.pas' {frmMain},
   uDMAboutBoxImage in 'uDMAboutBoxImage.pas' {DMAboutBoxImage: TDataModule},
   Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
   Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
-  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas';
+  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas',
+  fBuildForm in 'fBuildForm.pas' {frmBuildForm},
+  DelphiBooks.Classes in '..\lib-externes\DelphiBooks-Common\src\DelphiBooks.Classes.pas',
+  DelphiBooks.DB.Repository in '..\lib-externes\DelphiBooks-Common\src\DelphiBooks.DB.Repository.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TDMAboutBoxImage, DMAboutBoxImage);
-  Application.CreateForm(TfmrMain, fmrMain);
+  Application.CreateForm(TfrmMain, frmMain);
   Application.Run;
 end.
