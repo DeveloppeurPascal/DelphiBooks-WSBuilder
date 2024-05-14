2bbaf027d826ed4f5366e3121c4fbbe15e6fcfa5
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-14 12:53:56 +0200

    updated paths of the units in libraries and about box project

--- src/DelphiBooksWebSiteBuilder.dpr
+++ src/DelphiBooksWebSiteBuilder.dpr
@@ -5,14 +5,14 @@ uses
   FMX.Forms,
   fMain in 'fMain.pas' {frmMain},
   uDMAboutBoxImage in 'uDMAboutBoxImage.pas' {DMAboutBoxImage: TDataModule},
-  Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
-  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
-  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas',
   fBuildForm in 'fBuildForm.pas' {frmBuildForm},
   DelphiBooks.Classes in '..\lib-externes\DelphiBooks-Common\src\DelphiBooks.Classes.pas',
   DelphiBooks.DB.Repository in '..\lib-externes\DelphiBooks-Common\src\DelphiBooks.DB.Repository.pas',
   uBuilder in 'uBuilder.pas',
-  DelphiBooks.Tools in '..\lib-externes\DelphiBooks-Common\src\DelphiBooks.Tools.pas';
+  DelphiBooks.Tools in '..\lib-externes\DelphiBooks-Common\src\DelphiBooks.Tools.pas',
+  u_urlOpen in '..\lib-externes\librairies\src\u_urlOpen.pas',
+  Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialog.pas',
+  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm};
 
 {$R *.res}
 
